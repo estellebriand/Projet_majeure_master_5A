9a8dbd2e66c67b7fc3cbab0ed5bc99a36e334c67
estelleb <estelle.briand@cpe.fr>
2021-01-08 17:25:32 +0100

    test1 matlab ros, rosbag et manipulation ros device

--- matlab/network.m
+++ matlab/network.m
@@ -1,7 +1,6 @@
 %% Init
 %to share variable for calling callback function
-global pos
-global orient
+
 %set python environment
 pe = pyenv('Version','2.7');
 pe.Version
@@ -9,9 +8,20 @@ pe.Version
 %start roscore
 rosinit("192.168.1.54")
 rostopic list
-%r = rosrate(5)
-
-%while(r.TotalElapsedTime >10)
-%    %algo code
-%    waitfor(r) 
-%end
+getFile(d,'~/Bureau/projet_ws/bagfiles/bagfile.bag')
+bag = rosbag('bagfile.bag')
+ 
+% Display a list of the topics and message types in the bag file
+bag.AvailableTopics
+ 
+% Since the messages on topic turtle1/pose are of type Twist,
+% let's see some of the attributes of the Twist message
+msg_pose = rosmessage('turtlesim/Pose')
+showdetails(msg_pose)
+ 
+% Get just the topic we are interested in
+bagselect = select(bag,'Topic','turtle1/pose');
+ 
+% Create a time series object based on the fields of the turtlesim/Pose
+% message we are interested in
+ts = timeseries(bagselect,'X','Y','Theta','LinearVelocity','AngularVelocity');
---
+++ matlab/ros_matlab.md
@@ -0,0 +1,22 @@
+# Ros matlab and respberry pi
+Pour connecter ros matlab au roscore de la raspberry:
+- suivre network_ros.md pour configurer correctement la raspberry pi
+- raspberry -> launch roscore et les noeuds
+- Matlab -> ```rosinit("192.168.1.54")```
+
+## Get/Put file
+[Wiki matlab](https://fr.mathworks.com/help/ros/ref/putfile.html)
+
+Tout d'abord il faut se connecter en ssh pour pouvoir avoir accès au fichier. Avec le rosinit on sera connecter au roscore et avec rosdevice on sera comme en ssh sur la raspberry.
+
+Matlab:
+
+```matlab
+device = rosdevice('192.168.1.54','ros-ubuntu','projet2020')
+% Telecharger un fichier de la rasp vers le PC:
+getFile(d,'~/Bureau/projet_ws/bagfiles/bagfile.bag')
+% Envoyer un fichier du PC vers la rasp:
+putFile(d,'test_file.txt','~/Bureau/projet_ws')
+% Supprimer un fichier sur la rasp
+deleteFile(d,'~/Bureau/projet_ws/test_file.txt')
+```
