3b3f8f1dd54d3e73bb62e316a0fc634f017770dc
estelleb <estelle.briand@cpe.fr>
2021-01-11 18:07:27 +0100

    seance 11-01-2021 : raspberry side

---
+++ raspberry_pi/projet_ws/src/projet/script/publisher.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+from std_msgs.msg import String
+
+class talker():
+    def __init__(self):
+        
+        rospy.init_node('talker', anonymous=True)
+
+        pub = rospy.Publisher('/chatter', String, queue_size=10)
+        rate = rospy.Rate(10) # 10hz
+        while not rospy.is_shutdown():
+            hello_str = "hello world %s" % rospy.get_time()
+            #rospy.loginfo(hello_str)
+            pub.publish(hello_str)
+            rate.sleep()
+
+ 
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
+
