507497a6b451726ab934dbe2678e8d92e1a63ae2
estelleb <estelle.briand@cpe.fr>
2021-01-12 12:27:04 +0100

    test LIDAR matlab

--- matlab/chatter_callback.m
+++ matlab/chatter_callback.m
@@ -2,6 +2,7 @@ function chatter_callback(chatter, data)
 %CHATTER_CALLBACK Summary of this function goes here
 %   Detailed explanation goes here
 global variable 
+%data
 variable = data.Data;
 end
 
--- matlab/main_template.m
+++ matlab/main_template.m
@@ -1,6 +1,6 @@
 %% Init
 %to share variable for calling callback function
-global variable
+global scan
 %set python environment
 pe = pyenv('Version','2.7');
 % pe.Version
@@ -12,17 +12,31 @@ rostopic list
 %% subscribe /chatter topic
 % chatter_callback will update the global variable variable with
 % data.Data 
-%chatter = rossubscriber('/chatter', @chatter_callback);
-%pause(2) 
+
+talker_pub = rospublisher('/talker_matlab', 'std_msgs/String');
+chatter = rossubscriber('/chatter', 'std_msgs/String', @chatter_callback);
+
+laserscan = rossubscriber('/scan', 'sensor_msgs/LaserScan', @scan_callback);
+
+pause(2) 
 % we print the global variable
-%fprintf("%s",variable)
+%fprintf("%d",variable)
+% create and pub the msg
+talkermsg = rosmessage(talker_pub);
+talkermsg.Data = 'talker matlab';
+send(talker_pub,talkermsg);
+pause(2);
+
 
 %% call service /add_two_ints
-client = rossvcclient('/add_two_ints')
-req = rosmessage(client)
-req.a = 1
-req.b = 2
-resp = call(client,req,'Timeout',3);
+%client = rossvcclient('/add_two_ints')
+%req = rosmessage(client)
+%req.a = 1
+%req.b = 2
+%resp = call(client,req,'Timeout',3);
+
+%exampleHelperROSCreateSampleNetwork
+%testserver = rossvcserver('/test', 'std_srvs/Empty', @exampleHelperROSEmptyCallback)
 %% Rosbag
 %device = rosdevice('192.168.1.54','ros-ubuntu','projet2020')
 %getFile(device,'~/Bureau/projet_ws/bagfiles/bagfile.bag')
