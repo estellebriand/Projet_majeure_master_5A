4111f65f1a10f96f42320ad1c62a39108a232ac0
estelleb <estelle.briand@cpe.fr>
2021-01-20 20:27:53 +0100

    trie pour la demo

--- raspberry_pi/projet_ws/start_demo.sh
+++ raspberry_pi/projet_ws/start_demo.sh
@@ -5,17 +5,21 @@
 #set all the permission
 echo ============= Autorisation des ports USB =============
 sudo chmod 666 /dev/ttyUSB0
+sudo chmod 666 /dev/ttyUSB1
 #sudo chmod 666 /dev/ttyACM0
 echo ============= Check IP address =============
 
 ipadress=`hostname -I`
 echo $ipadress
-echo 'export ROS_HOSTNAME=192.168.1.54' >> ~/.bashrc
+#echo 'export ROS_HOSTNAME=192.168.1.54' >> ~/.bashrc
 #do a source
 source ~/.bashrc
 source ~/Bureau/projet_ws/devel/setup.bash
 
 #execute the launch file
-echo ============= Execute  roslaunch =============
-roslaunch rplidar_ros rplidar.launch 
-#rosrun projet publisher.py
+echo ============= Execute  roslaunch sensor =============
+#roslaunch rplidar_ros rplidar.launch 
+#roslaunch projet publisher.launch 
+#rosrun rosserial_python serial_node.py /dev/ttyUSB1
+roslaunch projet start_sensors.launch
+
